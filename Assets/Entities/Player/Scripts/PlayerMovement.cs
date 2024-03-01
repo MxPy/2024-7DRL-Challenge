78357c5d3287ce01af60fe79185582ac700b739e
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-03-01 23:28:03 +0100

    WIP

--- Assets/Entities/Player/Scripts/PlayerMovement.cs
+++ Assets/Entities/Player/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//TODO: Rename to PlayerController
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 2.5f;
@@ -11,16 +13,35 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
+        
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         //animator.SetFloat("Horizontal", movement.x);
         //animator.SetFloat("Vertical", movement.y);
         //animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+           shoot(0);
+        } 
+        else if (Input.GetKeyDown(KeyCode.UpArrow)){
+           shoot(1);
+        } 
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+           shoot(2);
+        } 
+        else if (Input.GetKeyDown(KeyCode.DownArrow)){
+           shoot(3);
+        } 
     }
 
     private void FixedUpdate() {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
         
     }
+
+    private void shoot(uint direction){
+
+    }
+
 }
