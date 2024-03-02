17a4a3fbf73041666d8d039fe911ff6e080cfbff
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-03-02 11:04:10 +0100

    add colision detection

--- Assets/Entities/Scripts/Bullet.cs
+++ Assets/Entities/Scripts/Bullet.cs
@@ -9,10 +9,23 @@ public class Bullet : MonoBehaviour
 
     public void Setup(Vector3 shootDir){
         this.shootDir = shootDir;
+        transform.eulerAngles = new Vector3(0,0, GetAngleFromVectorFloat(shootDir) -90);
     }
 
     private void Update() {
-        
         transform.position += shootDir * moveSpeed * Time.deltaTime;
+        Destroy(gameObject, 5f);
+    }
+
+    //move it to other file
+    public float GetAngleFromVectorFloat(Vector3 dir){
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if(n < 0) n += 360;
+        return n;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+            Destroy(gameObject);
     }
 }
