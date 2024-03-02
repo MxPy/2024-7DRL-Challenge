53713db4c7c48dbca65f7445d8028f3ca2ccad2c
buennoo <127790828+buennoo@users.noreply.github.com>
2024-03-02 18:15:58 +0100

    working camera switching
    it works!!!! pure joy

--- Assets/World/Scripts/Camera/SceneChange.cs
+++ Assets/World/Scripts/Camera/SceneChange.cs
@@ -7,6 +7,15 @@ public class SceneChange : MonoBehaviour
     public GameObject cameraPrefab;
 
     private void OnTriggerEnter2D(Collider2D other){
+        GameObject currentCamera = Camera.main.gameObject;
+
+        if (currentCamera.CompareTag("MainCamera"))
+        {
+            Destroy(currentCamera);
+            GameObject newCamera = Instantiate(cameraPrefab, transform.position, transform.rotation);
+            newCamera.tag = "MainCamera";
+        }
+        /*
         LevelChange level = cameraPrefab.GetComponent<LevelChange>();
         if (other.CompareTag("Player")){
             Debug.Log("bef function");
@@ -16,6 +25,7 @@ public class SceneChange : MonoBehaviour
         else{
             Debug.LogError("No script");
         }
+        */
     }
 
 }
