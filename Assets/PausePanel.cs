51e7b95ca99bb9e9a78fdb2df4680a96389b738e
buennoo <127790828+buennoo@users.noreply.github.com>
2024-03-05 20:35:32 +0100

    Add pause

---
+++ Assets/PausePanel.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanel : MonoBehaviour
+{
+    public GameObject pause;
+    private bool isPaused;
+    public AudioSource audioSource;
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        if(audioSource != null){
+            audioSource.Pause();
+        }
+
+        if(pause != null){
+            pause.SetActive(true);
+        }
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        if(audioSource != null){
+            audioSource.Play();
+        }
+
+        if (pause != null){
+            pause.SetActive(false);
+        }
+    }
+}
